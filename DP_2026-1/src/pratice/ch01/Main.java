884404067027d697c2c1ea015c4b66c5ccf2aef0
이수미 <tommygomi0621@naver.comgit config --global user.email>
2026-03-10 10:16:45 +0900

    0310

---
+++ DP_2026-1/src/pratice/ch01/Main.java
@@ -0,0 +1,8 @@
+package practice.ch01;
+
+public class Main {
+    public static void main(String[] args) {
+        Book book = new Book("자바의 정석");
+        System.out.println("Book name: " + book.getName());
+    }
+}
