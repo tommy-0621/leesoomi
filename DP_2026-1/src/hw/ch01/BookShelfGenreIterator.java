79ede2bbc623b6cf3fdc786eb44d07a677e31ec9
이수미 <tommygomi0621@naver.comgit config --global user.email>
2026-03-23 23:24:20 +0900

    ch01.과제완료

--- DP_2026-1/src/hw/ch01/BookShelfGenreIterator.java
+++ DP_2026-1/src/hw/ch01/BookShelfGenreIterator.java
@@ -4,36 +4,54 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class BookShelfGenreIterator implements Iterator<Book> {
-    private BookShelf bookShelf;
-    private String genre;
+    private Book[] filteredBooks;
     private int index;
 
     public BookShelfGenreIterator(BookShelf bookShelf, String genre) {
-        this.bookShelf = bookShelf;
-        this.genre = genre;
-        this.index = 0;
+        int length = bookShelf.getLength();
+
+       
+        int count = 0;
+        for (int i = 0; i < length; i++) {
+            if (bookShelf.getBookAt(i).getGenre().equals(genre)) {
+                count++;
+            }
+        }
+
+        
+        filteredBooks = new Book[count];
+        int k = 0;
+        for (int i = 0; i < length; i++) {
+            if (bookShelf.getBookAt(i).getGenre().equals(genre)) {
+                filteredBooks[k] = bookShelf.getBookAt(i);
+                k++;
+            }
+        }
+
+        
+        for (int i = 0; i < filteredBooks.length - 1; i++) {
+            for (int j = 0; j < filteredBooks.length - 1 - i; j++) {
+                if (filteredBooks[j].getYear() < filteredBooks[j + 1].getYear()) {
+                    Book temp = filteredBooks[j];
+                    filteredBooks[j] = filteredBooks[j + 1];
+                    filteredBooks[j + 1] = temp;
+                }
+            }
+        }
+
+        index = 0;
     }
 
     @Override
     public boolean hasNext() {
-        while(index < bookShelf.getLength()) {
-            Book book = bookShelf.getBookAt(index);
-            if (book.getGenre().equals(genre)) {
-                return true;
-            }
-            index++;
-        }
-            return false;
-        }
-    
+        return index < filteredBooks.length;
+    }
 
     @Override
     public Book next() {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        Book book = bookShelf.getBookAt(index);
-        index++;
-        return book;
+        return filteredBooks[index++];
     }
 }
